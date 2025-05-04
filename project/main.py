86273e34768058d54145966a41d2b63e2cf358e0
Sherif Okunade <o.d.alsherif@gmail.com>
2025-05-04 11:55:48 +0100

    added cors

--- project/main.py
+++ project/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 from .routes import users
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Allow CORS from your frontend origin
+# origins = [
+#     "http://localhost:3000",  # React dev server
+#     "https://your-frontend-domain.com"  # Replace with your deployed frontend URL
+# ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(users.router)
